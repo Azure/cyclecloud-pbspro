c8d7430ebadb265d30c1564ef34eb71f9bfa4ac9
Ryan Hamel <ryan.hamel@microsoft.com>
2021-12-03 10:07:16 -0500

    Fix bug with assigned/available shared resource calculation

--- pbspro/src/pbspro/parser.py
+++ pbspro/src/pbspro/parser.py
@@ -155,9 +155,9 @@ class PBSProParser:
             if res_name not in non_host_resources:
                 continue
 
-            current_value = res_assigned.get(res_name)
-            if current_value is None:
-                current_value = initial_value
+            assigned_value = res_assigned.get(res_name) or 0
+
+            current_value = initial_value  - assigned_value
 
             resource = non_host_resources[res_name]
 
