71762f05345c060544e18782b930134c99a5a6fa
Ryan Hamel <ryan.hamel@microsoft.com>
2024-01-26 13:54:12 -0500

    Bump version to 2.0.21

--- pbspro/setup.py
+++ pbspro/setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 from setuptools.command.test import Command
 from setuptools.command.test import test as TestCommand  # noqa: N812
 
-__version__ = "2.0.20"
+__version__ = "2.0.21"
 CWD = os.path.dirname(os.path.abspath(__file__))
 
 
--- specs/default/chef/site-cookbooks/pbspro/metadata.rb
+++ specs/default/chef/site-cookbooks/pbspro/metadata.rb
@@ -6,7 +6,7 @@ maintainer       "Microsoft Corporation"
 license          "MIT"
 description      "Installs/Configures Open PBS Pro"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.20"
+version          "2.0.21"
 depends          "tandem"
 %w{ cganglia cshared cuser cyclecloud }.each {|c| depends c}
 
