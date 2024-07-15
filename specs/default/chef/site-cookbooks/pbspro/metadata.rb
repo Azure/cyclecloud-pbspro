7cdac637cecd3d1828ea7c14e2436ec831ea2f4f
Ryan Hamel <ryan.hamel@microsoft.com>
2024-07-15 18:44:26 +0000

    bump version to 2.0.23

--- specs/default/chef/site-cookbooks/pbspro/metadata.rb
+++ specs/default/chef/site-cookbooks/pbspro/metadata.rb
@@ -6,7 +6,7 @@ maintainer       "Microsoft Corporation"
 license          "MIT"
 description      "Installs/Configures Open PBS Pro"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.22"
+version          "2.0.23"
 depends          "tandem"
 %w{ cganglia cshared cuser cyclecloud }.each {|c| depends c}
 
