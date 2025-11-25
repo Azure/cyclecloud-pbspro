dc952d5d54a857325c4747e4adaabda66481d1b2
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-11-25 10:41:13 +0100

    fix typo in script name in comments

--- server_dyn_res_wrapper.sh
+++ server_dyn_res_wrapper.sh
@@ -2,7 +2,7 @@
 # Example
 # server_dyn_res: "myres !/path/to/my/script.sh"
 # becomes
-# server_dyn_res: "myres !/opt/cycle/pbspro/serv_dyn_res_wrapper.sh myres /path/to/my/script.sh"
+# server_dyn_res: "myres !/opt/cycle/pbspro/server_dyn_res_wrapper.sh myres /path/to/my/script.sh"
 res_name=$1
 shift
 $@ > /opt/cycle/pbspro/server_dyn_res/$res_name
